b61de47dfcb371d4ca461f59ff318d10fb88b520
Saleh Osman <saleh.osman80@gmail.com>
2026-03-24 13:27:50 +0200

    feat(i18n-core): export public API

---
+++ packages/i18n-core/src/index.ts
@@ -0,0 +1,5 @@
+export { i18nConfig } from './i18n.config';
+export { loadModuleLocales } from './loader';
+export { t } from './t';
+export { getLocaleInfo } from './locale-info';
+export { sanitizeValue } from './sanitizer';
